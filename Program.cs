9117ec5450b0324894b0bdafbe9fbd47b3369884
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-21 12:43:58 +0530

    [Ariprasath] added: initialising player and computer symbols; 'X' or 'O'

--- Program.cs
+++ Program.cs
@@ -6,11 +6,32 @@ namespace TikTakToe
     {
         static void Main(string[] args)
         {
-            EmptyBoard();
+            //EmptyBoard();
+            LetterSelection();
         }
         public static void EmptyBoard()
         {
             char[] board = new char[10] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+        public static void LetterSelection()
+        {
+            char player;
+            char computer;
+            Console.WriteLine("Enter the letter yo want to choose 'X' and 'O'");
+            player = Convert.ToChar(Console.ReadLine());
+            if (player!='X' && player!='O')
+            {
+                Console.WriteLine("Please enter a valid input; Either 'X' or 'O'");
+                LetterSelection();
+            }
+            if (player == 'X')
+            {
+                computer = 'O';
+            }
+            else
+            {
+                computer = 'X';
+            }
         }
     }
 }
